19320fc67562d53e3c2ee02090b13183f4dd09a7
Lakshan Ruwinda <112312002+shan-ganegoda@users.noreply.github.com>
2022-11-09 23:06:27 +0530

    Dashboard Design
    Dashboard Design Partially Completed. Added Table component to Dashboard and Some Cards to It.Cookies can be added but can.t be deleted.that has to be fixed in next commit.

--- Login/logincontroller.php
+++ Login/logincontroller.php
@@ -2,6 +2,7 @@
 
 include_once("./commondao.php");
 
+
 //Check if the password or username is entered
 if(isset($_POST['username'])  && isset($_POST['password'])){
 
@@ -9,17 +10,22 @@ if(isset($_POST['username'])  && isset($_POST['password'])){
     $password = $_POST['password'];
 
     $sql = "SELECT * FROM user_credentials WHERE username = '$username' AND password = '$password' ";    
-    $result = mysqli_query($dbconn,$sql); 
+    $result = mysqli_query($dbconn,$sql);
+    
+   
 
     //if any user found what to do
     if(mysqli_num_rows($result) == 1){
         echo "Login Successfully";
-        header("Location: main.php");
+        header("Location: ../Main/main.php");
     }else{
         $em = "Invalid username or password";
         header("Location: login.php?error=$em");
     }
 
+    $cookie_name = "user";
+    $cookie_value = $username;
+    setcookie($cookie_name, $cookie_value, time() + (86400), "/");
 
 
 }
---
+++ Main/dashboard.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/dataTables.bootstrap5.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+<div class="row">
+          <div class="col-md-12">
+            <h4>Dashboard</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 mb-3">
+            <a href="#">
+            <div class="card bg-primary text-white h-100">
+              <div class="card-body py-5">Primary Card</div>
+              <div class="card-footer d-flex" >
+                View Details
+                <span class="ms-auto">
+                  <i class="bi bi-chevron-right"></i>
+                </span>
+              </div>
+            </div>
+            </a>
+          </div>
+          <div class="col-md-3 mb-3">
+            <div class="card bg-warning text-dark h-100">
+              <div class="card-body py-5">Warning Card</div>
+              <div class="card-footer d-flex">
+                View Details
+                <span class="ms-auto">
+                  <i class="bi bi-chevron-right"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 mb-3">
+            <div class="card bg-success text-white h-100">
+              <div class="card-body py-5">Success Card</div>
+              <div class="card-footer d-flex">
+                View Details
+                <span class="ms-auto">
+                  <i class="bi bi-chevron-right"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 mb-3">
+            <div class="card bg-danger text-white h-100">
+              <div class="card-body py-5">Danger Card</div>
+              <div class="card-footer d-flex">
+                View Details
+                <span class="ms-auto">
+                  <i class="bi bi-chevron-right"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
+    <script src="./js/jquery-3.5.1.js"></script>
+    <script src="./js/jquery.dataTables.min.js"></script>
+    <script src="./js/dataTables.bootstrap5.min.js"></script>
+    <script src="./js/script.js"></script>
+</body>
+</html>
